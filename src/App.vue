99abae36f2ca748975905077560fa5cc6d362773
DiegoFront-tech <cuentapremium343@gmail.com>
2024-09-12 17:23:55 +0200

    enlace correcto

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 		</button>
 		<div class="btns-container" :class="{ 'btns-container-open': open }">
 			<button class="btn open-image" @click="openModal()">Abrir</button>
-			<a href="./assets/Cartita.pdf" download class="btn download">Descargar</a>
+			<a href="../public/Cartita.pdf" download="Carta" class="btn download" type="pdf">Descargar</a>
 		</div>
 	</div>
 
